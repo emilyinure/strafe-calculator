5893cc2c816bf12a529d4dfe35386e7ee98c0c65
emilyinure <114822181+emilyinure@users.noreply.github.com>
2025-05-03 01:08:46 -0400

    another error

--- src/main.rs
+++ src/main.rs
@@ -50,7 +50,7 @@ fn main() {
             .start(
                 canvas,
                 web_options,
-                Box::new(|cc| Ok(Box::new(eframe_template::TemplateApp::new(cc)))),
+                Box::new(|cc| Ok(Box::new(strafe_calculator::TemplateApp::new(cc)))),
             )
             .await;
 
